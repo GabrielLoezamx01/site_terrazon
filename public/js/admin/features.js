93c436195e2794e8445ec6e33b0ec5bf3a4639d5
GabrielLoeza <gabriel26loeza04@gmail.com>
2024-06-25 23:30:08 -0600

    JS vue en public

---
+++ public/js/admin/features.js
@@ -0,0 +1,59 @@
+var api = "/admin/features";
+$(document).ready(function () {
+    $("#myTable").DataTable({
+        info: false,
+        paging: false,
+    });
+});
+const app = new Vue({
+    el: "#vueApp",
+    data: {
+        insertForm: true,
+        UpdateForm: false,
+        name: "",
+        svgFile: null,
+        id: null,
+        data: {},
+        alert: "",
+    },
+    methods: {
+        deleteshow: function (id) {
+            this.id = id;
+            this.alert = "¿Está seguro de eliminar ?";
+        },
+        showModal(option, id = null) {
+            this.UpdateForm = option;
+            if (this.UpdateForm) {
+                this.insertForm = false;
+                this.id = id;
+                this.showApi();
+            } else {
+                this.insertForm = true;
+            }
+            var modal = new bootstrap.Modal(
+                document.getElementById("staticBackdrop")
+            );
+            modal.show();
+        },
+        showApi: function () {
+            axios
+                .get(api + "/" + this.id)
+                .then((response) => {
+                    this.name = response.data.name;
+                })
+                .catch((error) => {
+                    alert(error);
+                });
+        },
+        eliminar: function () {
+            axios
+                .delete(api + "/" + this.id)
+                .then((response) => {
+                    window.location.reload();
+                })
+                .catch((error) => {
+                    alert(error);
+                });
+        },
+    },
+});
