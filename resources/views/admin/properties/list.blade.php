1f1a8df82a9a7bbbef906ded15e60ebde0ff7b73
GabrielLoezamx01 <gabriel26loeza04@gmail.com>
2024-07-23 04:18:38 -0600

    feat:

--- resources/views/admin/properties/list.blade.php
+++ resources/views/admin/properties/list.blade.php
@@ -9,6 +9,8 @@
 @section('content')
     <div class="page-header d-print-none">
         <div class="container-xl">
+
+
             <div class="row g-2 align-items-center">
                 <div class="col-auto ms-auto d-print-none">
                     <div class="btn-list">
@@ -29,6 +31,9 @@
                     </div>
                 </div>
             </div>
+
+
+            {{-- MEJORA DEL AMDIN LIST --}}
         </div>
     </div>
     <div class="page-body">
