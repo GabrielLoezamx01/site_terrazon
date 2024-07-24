b660ab321944b82708ec499cbd674f3d4b57f823
GabrielLoezamx01 <gabriel26loeza04@gmail.com>
2024-07-24 09:11:08 -0600

    feat(admin): :sparkles: filtros de home

---
+++ app/Http/Controllers/Site/FilterController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Site;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Property;
+class FilterController extends Controller
+{
+    public function property_searchin(Request $req){
+        
+    }
+}
