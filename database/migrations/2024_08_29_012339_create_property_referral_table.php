06b60220399b0634df80ef5b9ae3e47fc6e8f953
GabrielLoezamx01 <gabriel26loeza04@gmail.com>
2024-08-29 03:03:02 -0600

    bug migracion

--- database/migrations/2024_08_29_012339_create_property_referral_table.php
+++ database/migrations/2024_08_29_012339_create_property_referral_table.php
@@ -16,9 +16,8 @@ class CreatePropertyReferralTable extends Migration
         Schema::create('property_referral', function (Blueprint $table) {
             $table->id();
             $table->foreignId('property_id')->constrained('properties')->onDelete('cascade');
-            $table->foreignId('id_referral')->constrained('referrals')->onDelete('cascade');
+            $table->foreignId('id_referral')->constrained('referrals', 'id_referral')->onDelete('cascade');
             $table->timestamps();
-
             $table->unique('property_id');  // Evita que una propiedad sea asignada a más de un referido
         });
     }
