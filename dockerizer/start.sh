1c0de328507308bad66425f6f6c34f864556e037
MacBook Gbetus <gbetus@gmail.com>
2024-09-19 13:46:38 -0600

    CMD ./laravel-setup.sh && rm -rf /var/cache/nginx/*

--- dockerizer/start.sh
+++ dockerizer/start.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -e
  
-rm -rf /var/cache/nginx/* 
-./laravel-setup.sh
+rm -rf /var/cache/nginx/*  && ./laravel-setup.sh
 
 # Ejecutar el comando pasado al contenedor (en este caso, será supervisord)
 exec "$@"
