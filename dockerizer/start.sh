8052aae6da48e082777c655c7c6f4cabdd9a5f84
MacBook Gbetus <gbetus@gmail.com>
2024-09-19 12:36:12 -0600

    nginx logs remove

--- dockerizer/start.sh
+++ dockerizer/start.sh
@@ -4,8 +4,5 @@ set -e
 # Limpiar la caché de Nginx
 rm -rf /var/cache/nginx/*
 
-# Ejecutar el script de configuración de Laravel
-/app/laravel-setup.sh
-
 # Ejecutar el comando pasado al contenedor (en este caso, será supervisord)
 exec "$@"
