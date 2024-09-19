1769113b3c33020ad0d129dd1baec027be20155f
MacBook Gbetus <gbetus@gmail.com>
2024-09-19 12:31:47 -0600

    ENTRYPOINT

--- dockerizer/start.sh
+++ dockerizer/start.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+set -e
 
-php-fpm
-
-# Borra el caché de Nginx
+# Limpiar la caché de Nginx
 rm -rf /var/cache/nginx/*
 
-# Inicia Nginx en primer plano
-nginx -g 'daemon off;'
+# Ejecutar el script de configuración de Laravel
+/app/laravel-setup.sh
 
+# Ejecutar el comando pasado al contenedor (en este caso, será supervisord)
+exec "$@"
