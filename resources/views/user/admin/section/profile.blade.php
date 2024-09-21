83313e9f52b8e8261f742bbc54f3212a066bfc01
GabrielLoezamx01 <gabriel26loeza04@gmail.com>
2024-09-21 03:18:42 -0600

    adaptacion de btn

--- resources/views/user/admin/section/profile.blade.php
+++ resources/views/user/admin/section/profile.blade.php
@@ -9,28 +9,28 @@
           @include('user.admin.section.aditional')
           @include('user.admin.section.phone')
 
-         <div class="row">
-    <div class="col-12 col-md-10 m-3">
-        <div class="d-flex flex-wrap justify-content-between align-items-center">
-            <!-- Aviso de privacidad -->
-            <div class="text-start mb-2 mb-md-0">
-                <span class="privacy-link">
-                    Consulta nuestro <a href="{{ url('custom/privacy') }}">Aviso de Privacidad</a>
-                </span>
-            </div>
+          <div class="row">
+              <div class="col-12 col-md-10 m-3">
+                  <div class="row justify-content-between align-items-center">
+                      <div class="col-12 col-md-auto text-start mb-2 mb-md-0">
+                          <span class="privacy-link">
+                              Consulta nuestro <a href="{{ url('custom/privacy') }}">Aviso de Privacidad</a>
+                          </span>
+                      </div>
 
-            <!-- Botones -->
-            <div class="text-end text-md-end text-center mt-2 mt-md-0">
-                <button type="button" onclick="location.href=''" class="btn cancel-btn me-2">
-                    CANCELAR
-                </button>
-                <button type="submit" class="btn save-btn">
-                    GUARDAR
-                </button>
-            </div>
-        </div>
-    </div>
-</div>
+                      <div class="col-12 col-md-auto text-end mt-2 mt-md-0">
+                          <div class="d-flex justify-content-center justify-content-md-end">
+                              <button type="button" onclick="location.href=''" class="btn cancel-btn me-2">
+                                  CANCELAR
+                              </button>
+                              <button type="submit" class="btn save-btn">
+                                  GUARDAR
+                              </button>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
 
           <div class="p-3"></div>
       </form>
