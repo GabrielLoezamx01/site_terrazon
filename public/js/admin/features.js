3d1730b4943d3dfe444cbd6547e3c5c14eef0c33
MacBook Gbetus <gbetus@gmail.com>
2024-09-21 14:46:47 -0600

    stage

--- public/js/admin/features.js
+++ public/js/admin/features.js
@@ -1,53 +1,14 @@
-var api = "/admin/features";
-const app = new Vue({
-    el: "#vueApp",
-    data: {
-        insertForm: true,
-        UpdateForm: false,
-        name: "",
-        svgFile: null,
-        id: null,
-        data: {},
-        alert: "",
-    },
-    methods: {
-        deleteshow: function (id) {
-            this.id = id;
-            this.alert = "¿Está seguro de eliminar ?";
-        },
-        showModal(option, id = null) {
-            this.UpdateForm = option;
-            if (this.UpdateForm) {
-                this.insertForm = false;
-                this.id = id;
-                this.showApi();
-            } else {
-                this.insertForm = true;
-            }
-            var modal = new bootstrap.Modal(
-                document.getElementById("staticBackdrop")
-            );
-            modal.show();
-        },
-        showApi: function () {
-            axios
-                .get(api + "/" + this.id)
-                .then((response) => {
-                    this.name = response.data.name;
-                })
-                .catch((error) => {
-                    alert(error);
-                });
-        },
-        eliminar: function () {
-            axios
-                .delete(api + "/" + this.id)
-                .then((response) => {
-                    window.location.reload();
-                })
-                .catch((error) => {
-                    alert(error);
-                });
-        },
-    },
-});
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	
+/******/ 	
+/******/ })()
+;
